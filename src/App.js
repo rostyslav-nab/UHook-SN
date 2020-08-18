56469d7da76e393d85bf73ffa35d5fb58c08c590
Rostyslav Nabok <rostuslav.nab@gmail.com>
2020-08-18 14:57:23 +0300

    small refactor

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css'
 import {Header} from "./components/Header/Header"
 import {Navbar} from "./components/Navbar/Navbar"
 import {Profile} from "./components/Profile/Profile"
-import {Dialogs} from "./components/Dialogs/Dialogs";
-import {Route} from "react-router-dom";
+import {Dialogs} from "./components/Dialogs/Dialogs"
+import {Route} from "react-router-dom"
 
 export const App = () =>{
   return(
