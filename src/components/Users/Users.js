68a9ad12abe74f70cce6d2f436ed081416340c8c
Rostyslav Nabok <rostuslav.nab@gmail.com>
2020-08-30 13:50:51 +0300

    52 side effects

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,36 +4,45 @@ import * as axios from "axios"
 import noPhoto from '../../assets/notPhoto.png'
 
 export const Users = (props) => {
-        if(props.users.length === 0){
+
+    let getUsers = () => {
+        if (props.users.length === 0) {
             axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(res=> {
+                .then(res => {
                     props.setUsers(res.data.items)
                     console.log(res.data.items)
                 })
         }
 
+    }
+
     return (
         <div>
+            <button onClick={getUsers} className={'btn btn-primary'}>Get Users</button>
             {
-                props.users.map(user=>
-                        <div key={user.id} className={classes.usersList}>
-                            <div className={classes.userCard}>
-                                <img src={user.photos.small != null ? user.photos.small : noPhoto} alt="userAva"/>
-                                <div className={classes.userDescription}>
-                                    <h4>{user.name}</h4>
-                                    <div className={classes.userLocation}>
-                                        {/*<span>City: {user.location.city}</span>*/}
-                                        {/*<span>Country: {user.location.country}</span>*/}
-                                    </div>
-                                    <div className={classes.userStatus}>
-                                        <span>Status: {user.status != null ? user.status : 'no status...'}</span>
-                                    </div>
-                                    { user.followed
-                                        ? <button className={'btn btn-danger'} onClick={()=>{props.unFollow(user.id)}}>Unfollow</button>
-                                        : <button className={'btn btn-primary'} onClick={() => {props.follow(user.id)}}>Follow</button>}
+                props.users.map(user =>
+                    <div key={user.id} className={classes.usersList}>
+                        <div className={classes.userCard}>
+                            <img src={user.photos.small != null ? user.photos.small : noPhoto} alt="userAva"/>
+                            <div className={classes.userDescription}>
+                                <h4>{user.name}</h4>
+                                <div className={classes.userLocation}>
+                                    {/*<span>City: {user.location.city}</span>*/}
+                                    {/*<span>Country: {user.location.country}</span>*/}
                                 </div>
+                                <div className={classes.userStatus}>
+                                    <span>Status: {user.status != null ? user.status : 'no status...'}</span>
+                                </div>
+                                {user.followed
+                                    ? <button className={'btn btn-danger'} onClick={() => {
+                                        props.unFollow(user.id)
+                                    }}>Unfollow</button>
+                                    : <button className={'btn btn-primary'} onClick={() => {
+                                        props.follow(user.id)
+                                    }}>Follow</button>}
                             </div>
                         </div>
+                    </div>
                 )
             }
         </div>
